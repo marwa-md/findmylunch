f47851a3eafb7dd2b69a05e0ffaaadc45a887f56
Marwa Mahmoud <md.mrwa@gmail.com>
2022-11-27 03:36:38 +0200

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='fixed-bottom' >
-      <footer>Marwa Mahmoud - <a href='https://marwa-dev.herokuapp.com/' target="_blank" >https://marwa-dev.herokuapp.com/</a> </footer>
+      <footer>Marwa Mahmoud - <a href='https://marwa.onrender.com/' target="_blank" >https://marwa-dev.herokuapp.com/</a> </footer>
     </div>
   )
 }
